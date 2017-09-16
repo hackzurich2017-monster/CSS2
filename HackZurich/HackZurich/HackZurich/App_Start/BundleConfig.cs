7389e160c0180c18aca0ef36adef73be9e2ca7c8
EUROPE\Yasmin.Adir <yasmin.adir@gmail.com>
2017-09-16 17:20:17 +0300

    Integrate chat box -cont.

--- HackZurich/HackZurich/HackZurich/App_Start/BundleConfig.cs
+++ HackZurich/HackZurich/HackZurich/App_Start/BundleConfig.cs
@@ -20,6 +20,9 @@ namespace HackZurich
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                        "~/Scripts/chatbox.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
---
+++ HackZurich/HackZurich/HackZurich/Scripts/chatbox.js
@@ -0,0 +1,52 @@
+﻿debugger;
+$(document).on('click', '.panel-heading span.icon_minim', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).on('focus', '.panel-footer input.chat_input', function (e) {
+    var $this = $(this);
+    if ($('#minim_chat_window').hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $('#minim_chat_window').removeClass('panel-collapsed');
+        $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).on('click', '#btn-chat', function (e) {
+    debugger;
+    var message = $("#btn-input").val();
+    var sentMessage = $(".msg_container:last-child").clone();
+    $(sentMessage).find("p").text(message);
+    $(sentMessage).removeClass("base_sent");
+    $(sentMessage).removeClass("base_receive");
+    $(sentMessage).addClass("base_sent");
+    sentMessage.appendTo(".msg_container_base");
+    $("#btn-input").val("");
+    $.post("url", { data: message })
+        .done(function (data) {
+            debugger;
+            var recievedMsg = $(".msg_container:last-child").clone();
+            $(recievedMsg).find("p").text(data);
+            $(recievedMsg).removeClass("base_sent");
+            $(recievedMsg).removeClass("base_receive");
+            $(recievedMsg).addClass("base_receive");
+            recievedMsg.appendTo(".msg_container_base");
+        });
+    
+    /*var size = $(".chat-window:last-child").css("margin-left");
+    size_total = parseInt(size) + 400;
+    alert(size_total);
+    var clone = $("#chat_window_1").clone().appendTo(".container");
+    clone.css("margin-left", size_total);*/
+});
+$(document).on('click', '.icon_close', function (e) {
+    //$(this).parent().parent().parent().parent().remove();
+    $("#chat_window_1").remove();
+});
